7a39f065c308b8d02067a501dc71811a5f070e65
wolsos-v <mamaev757@gmail.com>
2022-06-19 20:18:18 +0300

    feat: add server

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import dotenv from 'dotenv';
+import http from 'http';
+import router from './router';
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5001;
+const server = http.createServer(router);
+
+server.listen(PORT);
